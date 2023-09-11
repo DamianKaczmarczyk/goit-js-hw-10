fa28984df40b305d782641c37317d8c938623c12
DamianKaczmarczyk <jokernick@o2.pl>
2023-09-11 11:58:18 +0200

    fixes

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,27 +3,21 @@ import Notiflix from 'notiflix';
 import { selectBreed, catInfoContener, loader, failure } from './myVariables';
 
 export function fetchBreeds() {
-  const urlBreed = 'https://api.thecatapi.com/v1/breeds';
-  const options = [];
-  return new Promise((resolve, reject) => {
-    fetch(urlBreed, options)
-      .then(response => {
-        if (!response.ok) reject(new Error('Request failed'));
-        return response.json();
-      })
-      .then(data => resolve(data))
-      .catch(() =>
-        reject(
-          catInfoContener.classList.add('is-hiden'),
-          loader.classList.add('is-hiden'),
-          selectBreed.classList.add('is-hiden'),
-          failure.classList.remove('is-hiden'),
-          Notiflix.Notify.failure(
-            'Oops! Something went wrong! Try reloading the page!'
-          )
-        )
+  const urlBreed = "https://api.thecatapi.com/v1/breeds";
+  return fetch(urlBreed)
+    .then((response) => {
+      if (!response.ok) throw new Error("Request failed");
+      return response.json();
+    })
+    .catch(() => {
+      catInfoContener.classList.add("is-hiden");
+      loader.classList.add("is-hiden");
+      selectBreed.classList.add("is-hiden");
+      failure.classList.remove("is-hiden");
+      Notiflix.Notify.failure(
+        "Oops! Something went wrong! Try reloading the page!"
       );
-  });
+    });
 }
 
 // breed cat end
